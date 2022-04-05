7c98fe1cac20afd75ad256dd6acc8c85efc91aa3
Braeden Wright <braeden.wright21@gmail.com>
2022-04-05 16:39:22 -0500

    main

--- wrangle_prep.py
+++ wrangle_prep.py
@@ -217,3 +217,58 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70
     threshold = int(round(prop_required_row*len(df.columns),0))
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
+
+
+
+
+# Mall stuff
+
+def get_db_url(database):
+    from env import host, user, password
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
+
+
+def outlier_function(df, cols, k):
+    #function to detect and handle oulier using IQR rule
+    for col in df[cols]:
+        q1 = df.annual_income.quantile(0.25)
+        q3 = df.annual_income.quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr     
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df
+
+
+
+def get_mall_customers(sql):
+    url = get_db_url('mall_customers')
+    mall_df = pd.read_sql(sql, url, index_col='customer_id')
+    return mall_df
+
+
+
+def wrangle_mall_df():
+    
+    # acquire data
+    sql = 'select * from customers'
+
+
+    # acquire data from SQL server
+    mall_df = get_mall_customers(sql)
+    
+    # handle outliers
+    mall_df = outlier_function(mall_df, ['age', 'spending_score', 'annual_income'], 1.5)
+    
+    # get dummy for gender column
+    dummy_df = pd.get_dummies(mall_df.gender, drop_first=True)
+    mall_df = pd.concat([mall_df, dummy_df], axis=1).drop(columns = ['gender'])
+    mall_df.rename(columns= {'Male': 'is_male'}, inplace = True)
+    # return mall_df
+    return mall_df
+    # split the data in train, validate and test
+    train, test = train_test_split(mall_df, train_size = 0.8, random_state = 123)
+    train, validate = train_test_split(train, train_size = 0.75, random_state = 123)
+    
+    return train, validate, test
